d964f21d5bbe2145c84cd4be172d8c6b1ea03f14
github username <github email address>
2017-05-14 18:48:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getRepositores() {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send();
+}
+
+function showRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const source = document.getElementById("repository-template").innerHTML;
+  const template = Handlebars.compile(source);
+  const repoList = template(repos)
+  document.getElementById("repositories").innerHTML = repoList;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial('authorPartial', document.getElementById("author-partial-template").innerHTML)
+});
